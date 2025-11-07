a85d0a279279e3577ef3cb51615ca0546991dd9b
Kimhagonos <francohagonos@gmail.com>
2025-11-07 03:19:17 +0000

    Commit

--- Hagonos.py
+++ Hagonos.py
@@ -8,24 +8,22 @@ game_running = True
 print(f"Find the treasure at ({treasure_x}, {treasure_y})!")
 
 while game_running:
-    move = input("Enter move (up/down/left/right): ")
-
-print(f"Player position: ({player_x}, {player_y})")
-while game_running:
-    move = input("Enter move (up/down/left/right)"):
+    print(f"Player position: ({player_x}, {player_y})")
+    move = input("Enter move (up/down/left/right): ").lower()
 
     if move == "right":
         player_x += 1
-    if move == "left":
-        player_x += 1
-    if move == "up":
-        player_x += 1
-    if move == "down":
-        player_x += 1
+    elif move == "left":
+        player_x -= 1
+    elif move == "up":
+        player_y += 1
+    elif move == "down":
+        player_y -= 1
+    else:
+        print("Invalid move. Try again.")
+        continue
 
-print(f"Score - You: {player_score}, Computer: {computer_score}\n")
-
-if player_score == 2:
-    print("You won the game!")
-else:
-    print("Computer won the game!")
+    # Check if treasure is found
+    if player_x == treasure_x and player_y == treasure_y:
+        print(f"You found the treasure at ({treasure_x}, {treasure_y})! ")
+        game_running = False
